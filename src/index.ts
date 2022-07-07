2f7ac090198a724dda52272394775b4dff9a49fc
Caroline LS <carolinelds@gmail.com>
2022-07-07 01:35:35 -0300

    feat: initial structure

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+import router from "./routes/index.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
+app.listen(4000, () => {
+  console.log("App online on port 4000");
+});
